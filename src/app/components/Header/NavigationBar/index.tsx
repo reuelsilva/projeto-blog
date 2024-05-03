8283650348cf433b8f1042c39bdea9cf513e7408
reuelsilva <reuelsilva19@gmail.com>
2024-05-03 09:33:52 -0300

    refactor: renomeando o componente ItemMenu para NavItem

--- src/app/components/Header/NavigationBar/index.tsx
+++ src/app/components/Header/NavigationBar/index.tsx
@@ -1,16 +1,16 @@
 import { ReactElement } from "react";
 import ToggleButton from "./ToggleButton";
-import ItemMenu from "./ItemMenu";
+import NavItem from "./NavItem";
 
 export default function NavigationBar(): ReactElement{
     return(
         <nav className="flex flex-col items-center justify-center gap-2.5 mt-5 sm:items-start">
             <ToggleButton/>
             <ul id="menu" className="hidden flex-col items-center justify-center gap-2.5 sm:flex sm:flex-row sm:p-2.5">
-                <ItemMenu to="/">Home</ItemMenu>
-                <ItemMenu to="#">Notícias</ItemMenu>
-                <ItemMenu to="#">Curiosidades</ItemMenu>
-                <ItemMenu to="#">Fale Conosco</ItemMenu>
+                <NavItem to="/">Home</NavItem>
+                <NavItem to="#">Notícias</NavItem>
+                <NavItem to="#">Curiosidades</NavItem>
+                <NavItem to="#">Fale Conosco</NavItem>
             </ul>
         </nav>
     )
