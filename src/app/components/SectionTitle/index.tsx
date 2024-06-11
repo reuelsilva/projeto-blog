07512b92a6f9ac58d5dfa50accffd4c36fd99022
reuelsilva <reuelsilva19@gmail.com>
2024-06-11 18:25:30 -0300

    feat: adicionando o componente SectionTitle

---
+++ src/app/components/SectionTitle/index.tsx
@@ -0,0 +1,12 @@
+import { anta } from "@/app/utils/font";
+import { ReactElement } from "react";
+
+interface SectionTitleProps{
+    children: React.ReactNode
+}
+
+export default function SectionTitle({children}: SectionTitleProps):ReactElement{
+    return(
+        <h2 className={`text-xl font-bold text-green-600 ${anta.className}`}>{children}</h2>     
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import { anta } from "./utils/font";
 import Card from "./components/Card";
+import SectionTitle from "./components/SectionTitle";
 
 export default function Home() {
   return (
     <main className="px-2.5 py-2.5 sm:px-16 sm:py-10 flex-1" style={{minHeight: "calc(100vh - 214px)"}}>
       <section>
-        <h2 className={`text-xl font-bold text-green-600 ${anta.className}`}>ARTIGOS</h2>
+        <SectionTitle>ARTIGOS</SectionTitle>
         <hr className="border-gray-100" />
         <div className="grid grid-cols-1 gap-2.5 place-items-center mt-6 sm:grid-cols-[repeat(auto-fill,256px)] sm:gap-5">
           <Card 
