a7b04dfef96d982c0ab31a8a0fd1dc7c3c2e145b
reuelsilva <reuelsilva19@gmail.com>
2025-07-26 11:23:02 -0300

    fix: corrigido erro de digitação no nome do componente Footer

--- src/app/components/Footer/index.tsx
+++ src/app/components/Footer/index.tsx
@@ -1,7 +1,7 @@
 import { ReactElement } from "react"
 import { bebas_neue } from "@/app/utils/font"
 
-export default function Foooter(): ReactElement{
+export default function Footer(): ReactElement{
     return(
         <footer className="flex py-6 bg-gray-200">
             <p className={`text-white text-lg mx-auto ${bebas_neue.className}`}>Curiosidades de Tecnologia</p>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Header from "./components/Header";
-import Foooter from "./components/Footer";
+import Footer from "./components/Footer";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -21,7 +21,7 @@ export default function RootLayout({
       <body className={`flex flex-col min-h-screen ${inter.className}`}>
         <Header/>
         {children}
-        <Foooter/>
+        <Footer/>
       </body>
     </html>
   );
