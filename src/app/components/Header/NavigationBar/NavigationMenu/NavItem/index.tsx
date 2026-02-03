5db19fbbe295e31f59f4226319482925b4529cca
reuelsilva <reuelsilva19@gmail.com>
2026-02-03 11:15:31 -0300

    feat: implementa a ação de recolher a barra de navegação ao clique no item de menu

--- src/app/components/Header/NavigationBar/NavigationMenu/NavItem/index.tsx
+++ src/app/components/Header/NavigationBar/NavigationMenu/NavItem/index.tsx
@@ -1,5 +1,6 @@
 import { ReactElement} from "react"
 import Link from "next/link"
+import toggleNavigationMenu from "@/app/utils/toggle-navigation-menu"
 
 interface NavItemProps{
     to: string,
@@ -9,7 +10,7 @@ interface NavItemProps{
 export default function NavItem({to, children}: NavItemProps): ReactElement{
     return(
         <li>
-            <Link href={to} className="text-green-100 font-bold sm:p-2 sm:hover:text-green-600 sm:hover:bg-green-200 sm:hover:rounded">{children}</Link>
+            <Link onClick={toggleNavigationMenu} href={to} className="text-green-100 font-bold sm:p-2 sm:hover:text-green-600 sm:hover:bg-green-200 sm:hover:rounded">{children}</Link>
         </li>
     )
 }
